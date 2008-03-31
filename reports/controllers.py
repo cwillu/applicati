8eab257dd630fd0189e111e486c16941f9a41ff4
cwillu <cwillu@dominubuntu.(none)>
2008-03-31 12:57:23 -0600

    Mon Mar 31 12:57:23 CST 2008

--- reports/controllers.py
+++ reports/controllers.py
@@ -640,6 +640,7 @@ class Wiki(object):
       inlineObject = meta.data
       presentation = Presentation()
       content = inlineObject.show(meta, prefix=prefix + extension, formatted=True) #XXX tuples, not list            
+      print content
       return dom.parseString(content).getElementsByTagName('body')[0].toxml()
         
 #      return findPage(page, name.split('/')).show(formatted=True, prefix=prefix+[name])
