fd57bd679dab802bb6ca88318853b9093b769c0c
cwillu <cwillu@dominubuntu.(none)>
2008-08-15 02:54:19 -0600

    Fri Aug 15 02:54:19 CST 2008

--- reports/web.py
+++ reports/web.py
@@ -828,8 +828,11 @@ def test():
   assert "OK" == urlopen('http://127.0.0.1:8080/%s/root' % signed).msg
   assert "OK" == urlopen('http://127.0.0.1:8080/%s/root/' % signed).msg
   
-  assert "OK" == urlopen('http://127.0.0.1:8080/broken/').msg
-  
+  try:
+    assert False, urlopen('http://127.0.0.1:8080/broken/').msg
+  except HTTPError, err:
+    assert False, err
+    
   assert "OK" == urlopen('http://127.0.0.1:8080/?op=save;data=[root]+qwerty12345678').msg
   assert "qwerty12345678" in urlopen('http://127.0.0.1:8080/').read()
   
