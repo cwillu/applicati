ab0e1f5e8e7ad0736dc2f6f09e710cc300215b20
cwillu <cwillu@dominubuntu.(none)>
2008-08-17 06:02:08 -0600

    Sun Aug 17 06:02:08 CST 2008

--- reports/builtins.py
+++ reports/builtins.py
@@ -55,6 +55,7 @@ class Wiki(object):
       name = match.group('name')
       link = name[:-1] if name.endswith('*') else name
       link = '/'.join(prefix+(link,))
+      print link
       return '<a href="%s/">%s</a>' % (urllib.quote(link), name)
       
     def inlineLink(match):
