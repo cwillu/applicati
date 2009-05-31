28c6402cf71106741ed6c33e0ee5fe649f39d1be
cwillu <cwillu@cwillu.com>
2009-05-31 10:40:14 -0600

    Deleted out some obsolete'ish builtins

--- reports/builtins.py
+++ reports/builtins.py
@@ -255,68 +255,6 @@ class XML(object):
     self.links[name] = id    
     meta.data = self    
         
-class Clone(object):
-  def __init__(self, source=""):
-    self.source = source
-    self.data = None
-    
-  def setSource(self, meta, source=""):
-    self.source = source
-    meta.data = self
-            
-  def show(self, meta):
-    if not self.data:
-      meta.data = self
-    
-    return self.data
-
-class Editor(Wiki):
-  def __init__(self, data=''):
-    self.data = data
-    self.links = {}
-    
-  def resolve(self, meta, name):
-    raise KeyError(name)
-    
-  def link(self, meta, name, id):
-    pass
-    
-  def list(self, meta):
-    pass
-  
-  def show(self, meta, formatted=False, prefix=None):
-    self.reset(meta)
-    return self.data
-
-  def reset(self, meta):    
-    self.data = _(
-      content=[ 
-        'Lorem Ipsum Dolor Sit Amet',
-        '<br />'.join('consectetuer adipiscing elit Curabitur semper'.split(' ')),
-        ' | '.join('Aliquam ullamcorper suscipit est'.split(' ')),
-        """<p>Mauris a pede at nulla auctor tristique. Aenean tortor libero, feugiat sed, eleifend nec, interdum vel, lacus. Nunc ultrices placerat arcu. Donec mi felis, vestibulum at, scelerisque eget, ultrices nec, mauris. Fusce scelerisque sodales odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis mauris tortor, elementum ut, mattis nec, semper vitae, eros. Suspendisse purus nisl, porta a, dapibus non, sollicitudin sit amet, urna. Nulla faucibus pharetra eros. Mauris velit tellus, tincidunt at, sollicitudin at, tempor eu, tortor. Etiam nisi. Etiam venenatis risus vitae arcu. Suspendisse dictum nisi sit amet dolor hendrerit lacinia.</p>
-
-<p>Proin mattis. Vestibulum rutrum eleifend ante. Aenean nunc pede, commodo in, fringilla id, dictum ac, pede. Curabitur et enim. Cras in lorem ut nisi auctor pretium. Vestibulum metus turpis, volutpat eget, sodales quis, ultricies aliquam, erat. Vestibulum vehicula mi eu pede. Maecenas pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis quis mauris. Proin vehicula sollicitudin felis. Nulla id velit. Praesent nec libero ac libero tempus pharetra. Mauris enim turpis, volutpat in, dignissim ac, rhoncus quis, est. Proin diam. Praesent euismod tristique tortor.</p>
-
-<p>Duis lobortis magna quis tellus. Morbi auctor, dui et luctus ultricies, neque dolor adipiscing lectus, elementum feugiat nisi tortor in erat. Aenean sed nisi id diam ultricies aliquet. Aenean eros nisi, fermentum sed, vehicula vitae, tristique sollicitudin, enim. Integer tempus cursus justo. Suspendisse nibh. Etiam suscipit ipsum sed sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur at eros. Donec venenatis. Curabitur augue dolor, condimentum mollis, pellentesque commodo, varius ultrices, purus.</p>""",
-        """Maecenas augue pede, venenatis id, facilisis a, pellentesque ut, nunc. Proin vulputate. Nullam mi. Etiam posuere turpis nec dolor. Quisque ut lorem sit amet tortor ultricies iaculis. Fusce placerat diam eu lorem. Aliquam neque arcu, consequat sit amet, laoreet ut, tincidunt vel, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis risus. Vivamus pellentesque purus sit amet urna. Morbi quis orci. Ut ipsum ligula, vehicula congue, molestie ac, interdum at, urna. Nam sed elit. Etiam faucibus, odio tristique dignissim consectetuer, purus urna ullamcorper leo, at molestie mi lorem eu erat. Cras a metus.""",
-        'Phasellus vehicula, ipsum vel commodo dapibus, turpis pede aliquam sapien, eu facilisis diam nisi ac augue.',
-        'Donec auctor dolor a elit. Pellentesque sollicitudin odio nec metus.',
-        ], 
-      grid=_(x=[120,640,240], y=[100,50,360,100,100]),      
-      dimensions=[
-        (_(x=0, y=0), _(x=3, y=1)), #h
-        (_(x=0, y=1), _(x=1, y=3)), #*
-        (_(x=1, y=1), _(x=3, y=2)), #t
-        (_(x=1, y=2), _(x=2, y=3)), #c
-        (_(x=2, y=2), _(x=3, y=4)), #s
-        (_(x=0, y=3), _(x=2, y=4)), #f
-        (_(x=0, y=4), _(x=3, y=5)), #@
-        ], 
-      )    
-          
-#  def save(self, meta, data='', links=None):    
-#    pass
         
-metaTypes = dict((name, eval(name)) for name in ('Wiki', 'User', 'CapRoot', 'Raw', 'XML', 'Constructor', 'AutoLogin', 'Clone', 'Editor'))
+metaTypes = dict((name, eval(name)) for name in ('Wiki', 'User', 'CapRoot', 'Raw', 'XML', 'Constructor', 'AutoLogin'. ))
 
